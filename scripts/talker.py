0447d007de27dbc8e827e90150ffc8c7da75deee
Akash Purandare <akash.p1997@gmail.com>
2018-02-05 19:13:06 +0530

    Changed talker.py for Pond Test

--- scripts/talker.py
+++ scripts/talker.py
@@ -4,15 +4,15 @@ from std_msgs.msg import Float64
 import geometry_msgs.msg
 
 def talker():
-    pub = rospy.Publisher('/asv/left_force', geometry_msgs.msg.Wrench, queue_size=10)
-    pub2 = rospy.Publisher('/asv/right_force', geometry_msgs.msg.Wrench, queue_size=10)
+    pub = rospy.Publisher('/asv/left_pwm', Float64, queue_size=10)
+    pub2 = rospy.Publisher('/asv/right_pwm', Float64, queue_size=10)
     #listener = tf.TransformListener()
     rospy.init_node('talker')
     rate = rospy.Rate(100) # 10hz
     while not rospy.is_shutdown():
         #trans, rot = listener.lookupTransform('/thruster_left', '/base_footprint', rospy.Time(0));
-        msg = geometry_msgs.msg.Wrench(geometry_msgs.msg.Vector3(1,0,0), geometry_msgs.msg.Vector3(0,0,0.1))
-        msg2 = geometry_msgs.msg.Wrench(geometry_msgs.msg.Vector3(1,0,0), geometry_msgs.msg.Vector3(0,0,-1))
+        msg = 1600
+        msg2 = 1600
         pub.publish(msg)
         pub2.publish(msg2)
         rate.sleep()

b63449d430e27f471f3262e8754c187a9229057b
Akash Purandare <akash.p1997@gmail.com>
2018-03-23 21:35:41 +0530

    Need to test the thruster controller for first testing

--- scripts/create_pwm.py
+++ scripts/create_pwm.py
@@ -6,6 +6,9 @@ import time
 linear = 0
 angular = 0
 
+c = 0.8 #Ratio of max back thrust to max forward thrust
+r = 0.3 #Difference between thruster and COB of boat
+
 def listener():
 	rospy.init_node("thuster_pwm")
 	rospy.Subscriber("/lin/command", Float64, callback, callback_args=True)
@@ -21,15 +24,34 @@ def callback(data, lin):
 		angular = data.data
 
 def publish():
-	global linear, angular
+	global linear, angular, c
 	rate = rospy.Rate(100)
-	pub_l = rospy.Publisher("/left_pwm", Float64, callback)
-	pub_r = rospy.Publisher("/right_pwm", Float64, callback)
+	pub_l = rospy.Publisher("/left_pwm", Float64, queue_size=10)
+	pub_r = rospy.Publisher("/right_pwm", Float64, queue_size=10)
 	while not rospy.is_shutdown():
-		left_pwm = 0.5*(linear-angular)+1500
-		right_pwm = 0.5*(linear+angular)+1500
-		pub_l.publish(left_pwm)
-		pub_r.publish(right_pwm)
+		if angular==0:
+			if linear>=0:
+				x = linear/2
+				y = linear/2
+			else:
+				x = -linear/(2*c)
+				y = -linear/(2*c)
+		elif angular>0:
+			x = (0.5/c)*(angular/0.3-linear)
+			y = (0.5)*(angular/0.3+linear)
+		else:
+			x = (0.5)*(angular/0.3+linear)
+			y = (0.5/c)*(angular/0.3-linear)
+		if x>400:
+			x = 400
+		if x<-400:
+			x = -400
+		if y>400:
+			y = 400
+		if y<-400:
+			y = 400
+		pub_l.publish(x+1500)
+		pub_r.publish(y+1500)
 		rate.sleep()
 
 listener()
--- scripts/split_vel.py
+++ scripts/split_vel.py
@@ -4,6 +4,7 @@ from std_msgs.msg import Float64
 import std_msgs.msg
 import geometry_msgs.msg
 import nav_msgs.msg
+import sensor_msgs.msg
 
 odom = [0,0]
 vel = [0,0]
@@ -11,16 +12,20 @@ THRUSTER_COB = 300 #Distance between thrusters and cob
 
 def listener():
 	rospy.init_node("accel_to_pwm")
-	rospy.Subscriber("/odom", nav_msgs.msg.Odometry, callback=pwm, callback_args=True)
-	rospy.Subscriber("/cmd_vel", geometry_msgs.msg.Twist, callback=pwm, callback_args=False)
+	rospy.Subscriber("/vel", geometry_msgs.msg.TwistStamped, callback=pwm, callback_args=0)
+	rospy.Subscriber("/cmd_vel", geometry_msgs.msg.Twist, callback=pwm, callback_args=1)
+	rospy.Subscriber("/imu_data", sensor_msgs.msg.Imu, callback=pwm, callback_args=2)
 	talker()
 	rospy.spin()
 
 def pwm(data, current):
-	if(current):
-		odom = [data.twist.twist.linear.x, data.twist.twist.angular.z]
-	else:
+	global odom, vel
+	if(current==0):
+		odom[0] = data.twist.linear.x
+	elif current==1:
 		vel = [data.linear.x, data.angular.z]
+	elif current==2:
+		odom[1] = data.angular_velocity.z
 
 
 def talker():
@@ -31,6 +36,7 @@ def talker():
 	ang_cmd = rospy.Publisher("/ang/cmd", std_msgs.msg.Float64, queue_size=10)
 	rate = rospy.Rate(100)
 	while not rospy.is_shutdown():
+		rospy.loginfo(str(odom)+" "+str(vel))
 		lin_odom.publish(odom[0])
 		ang_odom.publish(odom[1])
 		lin_cmd.publish(vel[0])
